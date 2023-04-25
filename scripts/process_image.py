47305a5b5a2b00c1954c7bedaecddcd675d4add4
Kendall Harter <kendallremna@protonmail.com>
2023-04-24 19:12:10 -0700

    Font display

---
+++ scripts/process_image.py
@@ -0,0 +1,71 @@
+# Takes in an image from the command line and outputs its representation
+# as a header
+
+import cv2
+import sys
+
+if len(sys.argv) != 4:
+   sys.exit(f'Usage: {sys.argv[0]} input_file output_file variable_name')
+
+def to_cpp_array(a_list: list[int]) -> str:
+   return str(a_list).replace("[", "{").replace("]", "};")
+
+def make_gba_color(r, g, b):
+   # Converts from from 0-255 range to GBA range
+   def conv(val):
+      # Apparently only 15-31 are really visible
+      # So use 14 as 0 and go from there
+      # Mess around with this eventually for better colors
+      low_val = 0
+      return int(low_val + round((31 - low_val) * val / 255))
+
+   # Either the documentation is wrong (doubtful), or there's some weird endian issue
+   # This seems to work, though
+   # Swapping the bytes around doesn't seem to fix it so ehhhh
+   return (conv(r) << 10) + (conv(g) << 5) + (conv(b) << 0)
+
+def process_image(image) -> (list[int], list[int]):
+   image_vals = []
+   image_pal = []
+   pal_map = {}
+
+   # Process in blocks of 8x8
+   assert len(image) % 8 == 0
+   for base_y in range(len(image) // 8):
+      assert len(image[0]) % 8 == 0
+      for base_x in range(len(image[0]) // 8):
+         for y in range(8):
+            add_value = False
+            prev_value = 0
+            for x in range(8):
+               r, g, b = image[base_y * 8 + y][base_x * 8 + x]
+               if (r, g, b) not in pal_map:
+                  pal_num = len(image_pal)
+                  pal_map[(r, g, b)] = pal_num
+                  image_pal.append(make_gba_color(r, g,  b))
+               if add_value:
+                  image_vals.append((pal_map[(r, g, b)] << 4) + prev_value)
+                  add_value = False
+               else:
+                  add_value = True
+                  prev_value = pal_map[(r, g, b)]
+
+   bit32_vals = []
+   for a, b, c, d in zip(*([iter(image_vals)] * 4)):
+      bit32_vals.append(int(d << 24 | c << 16 | b << 8 | a))
+
+   return bit32_vals, image_pal
+
+
+with open(sys.argv[2], 'w') as f:
+   vals, pal = process_image(cv2.imread(sys.argv[1]))
+   header_guard = sys.argv[3].upper()
+   name = sys.argv[3]
+   f.write(
+      f'#ifndef {header_guard}_IMAGE_DATA\n'
+      f'#define {header_guard}_IMAGE_DATA\n'
+      f'#include <cstdint>\n'
+      f'inline constexpr std::uint32_t {name}[]{to_cpp_array(vals)}\n'
+      f'inline constexpr std::uint16_t {name}_pal[]{to_cpp_array(pal)}\n'
+      f'#endif\n'
+   )
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,76 @@
 #include "gba.hpp"
 
-#include <array>
+#include "generated/font.hpp"
 
-[[gnu::section(".ewram")]] std::array<std::uint32_t, 240 * 160 / 2> buffer;
+#include <array>
+#include <utility>
+
+void dma3_copy(const std::uint32_t* src, std::uint32_t* dest, int count)
+{
+   using namespace gba::dma_opt;
+   gba::dma3.set_source(src);
+   gba::dma3.set_destination(dest);
+   gba::dma3.set_word_count(count);
+   gba::dma3.set_options(gba::dma_options{}
+                            .set(dest_addr_cntrl::increment)
+                            .set(source_addr_cntrl::increment)
+                            .set(repeat::off)
+                            .set(transfer_type::bits_32)
+                            .set(start_timing::immediate)
+                            .set(irq::disable)
+                            .set(enable::on));
+}
 
 int main()
 {
    using namespace gba::dma_opt;
    using namespace gba::lcd_opt;
+   volatile std::uint16_t* bg_palettes = (std::uint16_t*)0x5000000;
+   volatile std::uint32_t* tiles_data = (std::uint32_t*)0x6000000;
+   // copy the debug font tiles and palette
+   dma3_copy(font, (std::uint32_t*)tiles_data, std::ssize(font));
+   // dma3_copy(font_pal, (std::uint32_t)bg_palettes, std::ssize(font_pal));
+   // std::copy(std::begin(font), std::end(font), tiles_data);
+   std::copy(std::begin(font_pal), std::end(font_pal), bg_palettes);
    gba::lcd.set_options(gba::lcd_options{}
-                           .set(bg_mode{3})
+                           .set(bg_mode{0})
                            .set(forced_blank::off)
-                           .set(display_bg2::on)
-                           .set(display_obj::on)
+                           .set(display_bg1::on)
+                           .set(display_obj::off)
                            .set(display_window_0::off)
                            .set(display_window_1::off)
                            .set(display_window_obj::off)
                            .set(obj_char_mapping::one_dimensional));
-   std::uint16_t part_color = 0;
-   volatile std::uint32_t* vram = (std::uint32_t*)0x6000000;
-   gba::dma3.set_source(buffer.data());
-   gba::dma3.set_destination((void*)vram);
-   gba::dma3.set_word_count(std::size(buffer));
-   std::uint16_t color = 0x00000000;
 
+   const auto bg1_loc = [&](int x, int y) {
+      return ((volatile std::uint16_t*)((std::uint8_t*)tiles_data + 0x800 * 31 + x * 2 + y * 0x40));
+   };
+
+   // TODO: Document these & make them options in gba.hpp
+   //       This is setting video mode options
+   *(volatile std::uint16_t*)(0x4000008) = 0b0001'1110'0000'0011;
+   *(volatile std::uint16_t*)(0x400000A) = 0b0001'1111'0000'0010;
+   for (int y = 0; y != 20; ++y) {
+      for (int x = 0; x != 30; ++x) {
+         *bg1_loc(x, y) = 32;
+      }
+   }
+   const auto write_it = [&](const char* c, int x, int y) {
+      for (int i = 0; c[i] != '\0'; ++i) {
+         *bg1_loc(x + i, y) = c[i];
+      }
+   };
+   // for (int y = 0; y != 20; ++y) {
+   //    for (int x = 0; x != 30; ++x) {
+   //       *bg1_loc(x, y) = x + y * 30;
+   //    }
+   // }
+   write_it("The quick brown fox jumped", 0, 0);
+   write_it("over the lazy brown dog.", 0, 1);
+   write_it("!@#$%^&*()_+", 0, 2);
+   write_it("0123456789`'\"~", 0, 3);
    while (true) {
       // Wait for VBlank
       while (*(volatile std::uint16_t*)(0x4000006) < 160) {}
-      for (auto& b : buffer) {
-         b = (color << 16) | color;
-      }
-      // We can't just enable reload because the source continues to increment
-      // through each iteration of the DMA if so
-      gba::dma3.set_options(gba::dma_options{}
-                               .set(dest_addr_cntrl::increment)
-                               .set(source_addr_cntrl::increment)
-                               .set(repeat::off)
-                               .set(transfer_type::bits_32)
-                               .set(start_timing::vblank)
-                               .set(irq::disable)
-                               .set(enable::on));
-      part_color += 1;
-      part_color %= 16;
-      color = (part_color << 11) | (part_color << 6) | (part_color << 1);
    }
 }
