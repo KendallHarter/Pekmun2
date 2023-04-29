3769e6cbae48b7b4b83d499fee65d536c40a49b3
Kendall Harter <kendallremna@protonmail.com>
2023-04-28 20:54:21 -0700

    More map generation + layering testing

--- py_experiments/test_layers.py
+++ py_experiments/test_layers.py
@@ -1,75 +1,97 @@
 import pygame
 import sys
 import json
-
-pygame.init()
-SCREEN_WIDTH = 240
-SCREEN_HEIGHT = 160
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
-
-tileset = pygame.image.load('../assets/test_tileset.png')
-tileset.set_colorkey(tileset.get_at((0, 0)))
-
-snake = pygame.image.load('../assets/snake.png')
-snake.set_colorkey(snake.get_at((0, 0)))
-
-cursor = pygame.image.load('../assets/cursor.png')
-cursor.set_colorkey(cursor.get_at((0, 0)))
-
-move_indic = pygame.image.load('../assets/py_move_indicator.png')
-move_indic.set_colorkey(move_indic.get_at((0, 0)))
-
-my_font = pygame.font.SysFont('Consolas', 12)
-
-with open('../assets/test_map2.json') as f:
-   map_info = json.load(f)
-
-map_width = map_info['width']
-map_height = map_info['height']
-
-def draw_tile_layer(layer_number):
-   for y in range(map_height):
-      for x in range(map_width):
-         loc = x + y * map_width
-         tile = map_info['layers'][layer_number]['data'][loc]
-         if tile != 0:
-            screen.blit(tileset, (x * 8, y * 8), (((tile - 1) * 8, 0), (8, 8)))
-
-# I don't know why but the Y offset needs to be somewhat adjusted at times
-y_adj = -2
-y_offset = 11
-
-map_layer_data = [
-   [True,  False, False, False, False],
-   [True,  True,  True,  False, False],
-   [True,  True,  True,  False, False],
-   [True,  False, False, False, False],
-   [False, False, False, False, False]
-]
-
-map_height_data = [
-   [3, 1, 1, 5, 7],
-   [1, 1, 2, 4, 7],
-   [1, 1, 2, 3, 7],
-   [1, 1, 1, 2, 7],
-   [1, 1, 1, 1, 7]
-]
-
-map_walkable = [
-   [1, 1, 1, 1, 1],
-   [1, 0, 1, 1, 1],
-   [1, 0, 1, 1, 0],
-   [1, 0, 1, 1, 1],
-   [1, 1, 1, 1, 1]
-]
-
-data_height = len(map_height_data)
-data_width = len(map_height_data[0])
+import os
 
 def clamp(n, smallest, largest):
    return max(smallest, min(n, largest))
 
 def main():
+   # Move to the directory the script is in so it can be run anywhere
+   os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+   pygame.init()
+   SCREEN_WIDTH = 240
+   SCREEN_HEIGHT = 160
+   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
+
+   tileset = pygame.image.load('../assets/test_tileset.png')
+   tileset.set_colorkey(tileset.get_at((0, 0)))
+
+   snake = pygame.image.load('../assets/snake.png')
+   snake.set_colorkey(snake.get_at((0, 0)))
+
+   cursor = pygame.image.load('../assets/cursor.png')
+   cursor.set_colorkey(cursor.get_at((0, 0)))
+
+   move_indic = pygame.image.load('../assets/py_move_indicator.png')
+   move_indic.set_colorkey(move_indic.get_at((0, 0)))
+
+   my_font = pygame.font.SysFont('Consolas', 12)
+
+   y_adj = -24
+   if len(sys.argv) == 1:
+      with open('../assets/test_map2.json') as f:
+         map_info = json.load(f)
+
+      y_offset = 11
+
+      sprite_priority = [
+         [True,  False, False, False, False],
+         [True,  True,  True,  False, False],
+         [True,  True,  True,  False, False],
+         [True,  False, False, False, False],
+         [False, False, False, False, False]
+      ]
+
+      tile_priority = [
+         [True,  False, False, False, False],
+         [True,  True,  True,  False, False],
+         [True,  True,  True,  False, False],
+         [True,  False, False, False, False],
+         [False, False, False, False, False]
+      ]
+
+      map_height_data = [
+         [3, 1, 1, 5, 7],
+         [1, 1, 2, 4, 7],
+         [1, 1, 2, 3, 7],
+         [1, 1, 1, 2, 7],
+         [1, 1, 1, 1, 7]
+      ]
+
+      map_walkable = [
+         [1, 1, 1, 1, 1],
+         [1, 0, 1, 1, 1],
+         [1, 0, 1, 1, 0],
+         [1, 0, 1, 1, 1],
+         [1, 1, 1, 1, 1]
+      ]
+
+      data_height = len(map_height_data)
+      data_width = len(map_height_data[0])
+   else:
+      with open(f'../assets/maps/{sys.argv[1]}/tiles.json') as f:
+         map_info = json.load(f)
+
+      with open(f'../assets/maps/{sys.argv[1]}/info.json') as f:
+         info = json.load(f)
+
+      sprite_priority = info['sprite_priority']
+      tile_priority = info['tile_priority']
+      y_offset = info['y_offset']
+      data_width = info['width']
+      data_height = info['height']
+
+      with open(f'../assets/raw_maps/{sys.argv[1]}.json') as f:
+         info = json.load(f)
+
+      map_height_data = info['heights']
+      map_walkable = info['walkable']
+
+   map_width = map_info['width']
+   map_height = map_info['height']
+
    snake_x = 0
    snake_y = 0
    cursor_x = 0
@@ -77,12 +99,22 @@ def main():
    selected = False
    move_layer_0 = []
    move_layer_1 = []
+   camera_x = 0
+   camera_y = 0
+
+   def draw_tile_layer(layer_number):
+      for y in range(map_height):
+         for x in range(map_width):
+            loc = x + y * map_width
+            tile = map_info['layers'][layer_number]['data'][loc]
+            if tile != 0:
+               screen.blit(tileset, (x * 8 - camera_x, y * 8 - camera_y), (((tile - 1) * 8, 0), (8, 8)))
 
    def draw_snake():
-      screen.blit(snake, (8 + snake_x * 16 + snake_y * 16, y_adj + y_offset * 6 + snake_y * 8 - 8 * snake_x - 8 * map_height_data[snake_y][snake_x]))
+      screen.blit(snake, (8 + snake_x * 16 + snake_y * 16 - camera_x, y_adj + y_offset * 8 + snake_y * 8 - 8 * snake_x - 8 * map_height_data[snake_y][snake_x] - camera_y))
 
    def draw_cursor():
-      screen.blit(cursor, (cursor_x * 16 + cursor_y * 16, y_adj + y_offset * 6 + 7 + cursor_y * 8 - 8 * cursor_x - 8 * map_height_data[cursor_y][cursor_x]))
+      screen.blit(cursor, (cursor_x * 16 + cursor_y * 16 - camera_x, y_adj + y_offset * 8 + 7 + cursor_y * 8 - 8 * cursor_x - 8 * map_height_data[cursor_y][cursor_x] - camera_y))
 
    while True:
       for event in pygame.event.get():
@@ -99,6 +131,15 @@ def main():
       elif keys[pygame.K_DOWN]:
          cursor_y += 1
 
+      if keys[pygame.K_w]:
+         camera_y -= 4
+      elif keys[pygame.K_s]:
+         camera_y += 4
+      if keys[pygame.K_a]:
+         camera_x -= 4
+      elif keys[pygame.K_d]:
+         camera_x += 4
+
       cursor_x = clamp(cursor_x, 0, data_width - 1)
       cursor_y = clamp(cursor_y, 0, data_height - 1)
 
@@ -107,7 +148,7 @@ def main():
             for y in range(data_height):
                for x in range(data_width):
                   if map_walkable[y][x]:
-                     if map_layer_data[y][x] == 0:
+                     if tile_priority[y][x] == 0:
                         move_layer_0.append((x, y))
                      else:
                         move_layer_1.append((x, y))
@@ -120,13 +161,13 @@ def main():
                move_layer_1 = []
                selected = False
 
-      snake_priority = map_layer_data[snake_y][snake_x]
-      cursor_priority = map_layer_data[cursor_y][cursor_x]
+      snake_priority = sprite_priority[snake_y][snake_x]
+      cursor_priority = sprite_priority[cursor_y][cursor_x]
 
       screen.fill((0, 0xff, 0))
       draw_tile_layer(0)
       for x, y in move_layer_0:
-         screen.blit(move_indic, (x * 16 + y * 16, y_adj + y_offset * 6 + 24 + 8 * y - 8 * x - 8 * map_height_data[y][x]))
+         screen.blit(move_indic, (x * 16 + y * 16 - camera_x, y_adj + y_offset * 8 + 24 + 8 * y - 8 * x - 8 * map_height_data[y][x] - camera_y))
       if cursor_priority == 0:
          draw_cursor()
       if snake_priority == 0:
@@ -134,14 +175,14 @@ def main():
 
       draw_tile_layer(1)
       for x, y in move_layer_1:
-         screen.blit(move_indic, (x * 16 + y * 16, y_adj + y_offset * 6 + 24 + 8 * y - 8 * x - 8 * map_height_data[y][x]))
+         screen.blit(move_indic, (x * 16 + y * 16 - camera_x, y_adj + y_offset * 8 + 24 + 8 * y - 8 * x - 8 * map_height_data[y][x] - camera_y))
       if cursor_priority == 1:
          draw_cursor()
       if snake_priority == 1:
          draw_snake()
 
-      screen.blit(my_font.render(f'{map_layer_data[cursor_y][cursor_x]}', False, (0, 0, 0)), (0, 0))
-      screen.blit(my_font.render(f'{cursor_x}, {cursor_y}', False, (0, 0, 0)), (0, 12))
+      # screen.blit(my_font.render(f'{map_layer_data[cursor_y][cursor_x]}', False, (0, 0, 0)), (0, 0))
+      screen.blit(my_font.render(f'{cursor_x}, {cursor_y}', False, (0, 0, 0)), (0, 0))
 
       pygame.display.flip()
       pygame.time.wait(50)
--- scripts/make_map.py
+++ scripts/make_map.py
@@ -1,68 +1,77 @@
 # Converts from a height/walkable map to a complete map
 
 import json
+import sys
+import os
 
-# temporary input
-HEIGHT_INPUT = [
-   [3, 1, 1, 5, 7],
-   [1, 1, 2, 4, 7],
-   [1, 1, 2, 3, 7],
-   [1, 1, 1, 2, 7],
-   [1, 1, 1, 1, 7]
-]
+# Max input width; this is so map data can always be padded to the same length
+# for quick multiplication
+MAX_INPUT_WIDTH = 16
 
-WALKABLE_INPUT = [
-   [1, 1, 1, 1, 1],
-   [1, 0, 1, 1, 1],
-   [1, 0, 1, 1, 0],
-   [1, 0, 1, 1, 1],
-   [1, 1, 1, 1, 1]
-]
+class Map:
+   def __init__(self, input_file):
+      with open(os.path.join('../assets/raw_maps/', input_file)) as f:
+         info = json.load(f)
 
-INPUT_WIDTH = len(HEIGHT_INPUT[0])
-INPUT_HEIGHT = len(HEIGHT_INPUT)
+      self.HEIGHT_INPUT = info['heights']
+      self.WALKABLE_INPUT = info['walkable']
+      self.INPUT_WIDTH = len(self.HEIGHT_INPUT[0])
+      self.INPUT_HEIGHT = len(self.HEIGHT_INPUT)
 
-assert len(HEIGHT_INPUT) == len(WALKABLE_INPUT)
-assert len(HEIGHT_INPUT[0]) == len(WALKABLE_INPUT[0])
+      assert self.INPUT_WIDTH <= MAX_INPUT_WIDTH
+      assert len(self.HEIGHT_INPUT) == len(self.WALKABLE_INPUT)
+      assert len(self.HEIGHT_INPUT[0]) == len(self.WALKABLE_INPUT[0])
 
-# This priority is for the tiles themselves, the sprite priority map
-# is somewhat different
-# A tile needs to be on a higher priority layer if:
-#     A tile of lower height is to the upper-right [x + 1]
-#     A tile of higher priority and height is to the upper-right [x + 1] (maybe?)
-#     A tile of lower height is to the upper-left [y - 1]
-def tile_is_high_priority(x, y):
-   if x < INPUT_WIDTH - 1:
-      if HEIGHT_INPUT[y][x + 1] < HEIGHT_INPUT[y][x]:
-         return True
-      # elif HEIGHT_INPUT[y][x + 1] > HEIGHT_INPUT[y][x] and tile_is_high_priority(x + 1, y):
-      #    return True
-   if y > 0 and HEIGHT_INPUT[y - 1][x] < HEIGHT_INPUT[y][x]:
-      return True
-   return False
-
-
-# A tile is high sprite priority if it is tile high priority or:
-#     A tile to the upper-left is tile high priority [y - 1]
-#     A tile to the upper-right is tile high priority [x + 1]
-#     A tile above one or two tiles is tile high priority [x + n, y - n]
-#     A tile one to the upper-left and two to the upper-right is tile high priority [y - 1, x + 2]
-def tile_is_sprite_priority(x, y):
-   if tile_is_high_priority(x, y):
-      return True
-   elif y > 0 and tile_is_high_priority(x, y - 1):
-      return True
-   elif x < INPUT_WIDTH - 1 and tile_is_high_priority(x + 1, y):
-      return True
-   elif y - 1 > 0 and x + 2 < INPUT_WIDTH - 1 and tile_is_high_priority(x + 2, y - 1):
-      return True
-   for i in range(1, 3):
-      if y - i > 0 and x + i < INPUT_WIDTH - 1:
-         if tile_is_high_priority(x + i, y - i):
+   # This priority is for the tiles themselves, the sprite priority map
+   # is somewhat different
+   # A tile needs to be on a higher priority layer if:
+   #     A tile of lower height is to the upper-right [x + 1]
+   #     A tile of higher priority and height is to the upper-right [x + 1] (maybe?)
+   #     A tile of lower height is to the upper-left [y - 1]
+   def tile_is_high_priority(self, x, y):
+      if x < self.INPUT_WIDTH - 1:
+         if self.HEIGHT_INPUT[y][x + 1] < self.HEIGHT_INPUT[y][x]:
             return True
-   return False
+         # elif HEIGHT_INPUT[y][x + 1] > HEIGHT_INPUT[y][x] and tile_is_high_priority(x + 1, y):
+         #    return True
+      if y > 0 and self.HEIGHT_INPUT[y - 1][x] < self.HEIGHT_INPUT[y][x]:
+         return True
+      return False
+
+
+   # A tile is high sprite priority if it is tile high priority or:
+   #     A tile to the upper-left is tile high priority [y - 1]
+   #     A tile to the upper-right is tile high priority [x + 1]
+   #     A tile above one or two tiles is tile high priority [x + n, y - n]
+   #     A tile one to the upper-left and two to the upper-right is tile high priority [y - 1, x + 2]
+   def tile_is_sprite_priority(self, x, y):
+      if self.tile_is_high_priority(x, y):
+         return True
+      elif y > 0 and self.tile_is_high_priority(x, y - 1):
+         return True
+      elif x < self.INPUT_WIDTH - 1 and self.tile_is_high_priority(x + 1, y):
+         return True
+      elif y - 1 > 0 and x + 2 < self.INPUT_WIDTH - 1 and self.tile_is_high_priority(x + 2, y - 1):
+         return True
+      for y_off in range(1, 3):
+         for x_off in range(1, 3):
+            if y - y_off > 0 and x + x_off < self.INPUT_WIDTH:
+               if self.tile_is_high_priority(x + x_off, y - y_off):
+                  return True
+      return False
 
 def main():
+   if len(sys.argv) != 2:
+      sys.exit(f'Usage: {sys.argv[0]} input_file*\n\t*Relative to ../assets/raw_maps')
+
+   # Move to the directory the script is in so it can be run anywhere
+   os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+   input_file = sys.argv[1]
+   output_dir = os.path.splitext(os.path.basename(sys.argv[1]))[0]
+
+   the_map = Map(input_file)
+
    BLANK_TILE = 18
    MAP_WIDTH = 60
    MAP_HEIGHT = 40
@@ -72,22 +81,29 @@ def main():
    # Find the lowest height the y-offset is known
    # The lowest x-offset is always 0
    min_y = float('inf')
-   for y in range(INPUT_HEIGHT):
-      for x in range(INPUT_WIDTH):
-         tile_y = y - x - HEIGHT_INPUT[y][x]
+   max_y = float('-inf')
+   for y in range(the_map.INPUT_HEIGHT):
+      for x in range(the_map.INPUT_WIDTH):
+         tile_y = y - x - the_map.HEIGHT_INPUT[y][x]
          min_y = min(min_y, tile_y)
+         max_y = max(max_y, tile_y)
 
    y_offset = -min_y
-   print(f'size: {INPUT_WIDTH}x{INPUT_HEIGHT}')
+   print(f'size: {the_map.INPUT_WIDTH}x{the_map.INPUT_HEIGHT}')
    print(f'y offset: {y_offset}')
-   for y in range(INPUT_HEIGHT):
-      for x in range(INPUT_WIDTH):
-         if WALKABLE_INPUT[y][x]:
+   max_x = (the_map.INPUT_WIDTH - 1) * 2 + (the_map.INPUT_HEIGHT - 1) * 2
+   if max_x >= MAP_WIDTH:
+      sys.exit(f'Max x {max_x} exceeds map size')
+   elif max_y >= MAP_HEIGHT:
+      sys.exit(f'Max y {max_y} exceeds map size')
+   for y in range(the_map.INPUT_HEIGHT):
+      for x in range(the_map.INPUT_WIDTH):
+         if the_map.WALKABLE_INPUT[y][x]:
             adjust_layer = low_priority
-            if tile_is_high_priority(x, y):
+            if the_map.tile_is_high_priority(x, y):
                adjust_layer = high_priority
             tile_x = x * 2 + y * 2
-            tile_y = y_offset + y - x - HEIGHT_INPUT[y][x]
+            tile_y = y_offset + y - x - the_map.HEIGHT_INPUT[y][x]
             # Draw the tile
             for i in range(4):
                # If there's already something there, write a tile that
@@ -100,22 +116,22 @@ def main():
                adjust_layer[tile_x + (tile_y + 1) * MAP_WIDTH + i] = i + 22
             # write the columns (only on the low priority layer)
             if adjust_layer is low_priority:
-               for i in range(HEIGHT_INPUT[y][x] - 1):
+               for i in range(the_map.HEIGHT_INPUT[y][x] - 1):
                   if adjust_layer[tile_x + (tile_y + i + 2) * MAP_WIDTH] == BLANK_TILE:
                      adjust_layer[tile_x + (tile_y + i + 2) * MAP_WIDTH] = 19
                      adjust_layer[tile_x + (tile_y + i + 2) * MAP_WIDTH + 1] = 19
                      adjust_layer[tile_x + (tile_y + i + 2) * MAP_WIDTH + 2] = 19
                      adjust_layer[tile_x + (tile_y + i + 2) * MAP_WIDTH + 3] = 19
             # Write the bottom on corner tiles
-            y_off = HEIGHT_INPUT[y][x] - 1 + 2
+            y_off = the_map.HEIGHT_INPUT[y][x] - 1 + 2
             if x == 0:
-               if adjust_layer[tile_x + (tile_y + y_off) * MAP_WIDTH] == BLANK_TILE:
-                  adjust_layer[tile_x + (tile_y + y_off) * MAP_WIDTH] = 14
-                  adjust_layer[tile_x + (tile_y + y_off) * MAP_WIDTH + 1] = 15
-            if y == INPUT_HEIGHT - 1:
-               if adjust_layer[tile_x + (tile_y + y_off) * MAP_WIDTH + 2] == BLANK_TILE:
-                  adjust_layer[tile_x + (tile_y + y_off) * MAP_WIDTH + 2] = 16
-                  adjust_layer[tile_x + (tile_y + y_off) * MAP_WIDTH + 3] = 17
+               if low_priority[tile_x + (tile_y + y_off) * MAP_WIDTH] == BLANK_TILE:
+                  low_priority[tile_x + (tile_y + y_off) * MAP_WIDTH] = 14
+                  low_priority[tile_x + (tile_y + y_off) * MAP_WIDTH + 1] = 15
+            if y == the_map.INPUT_HEIGHT - 1:
+               if low_priority[tile_x + (tile_y + y_off) * MAP_WIDTH + 2] == BLANK_TILE:
+                  low_priority[tile_x + (tile_y + y_off) * MAP_WIDTH + 2] = 16
+                  low_priority[tile_x + (tile_y + y_off) * MAP_WIDTH + 3] = 17
 
    # Emulate the Tiled file format
    # It discards extra fields when edited so a separate file will be needed for the
@@ -137,7 +153,7 @@ def main():
       'tilesets': [{
          'columns': 28,
          'firstgid': 1,
-         'image': 'test_tileset.png',
+         'image': '../../test_tileset.png',
          'imageheight': 8,
          'imagewidth': 224,
          'margin': 0,
@@ -174,12 +190,21 @@ def main():
       }]
    }
 
-   # print(to_cpp_array(low_priority))
-   with open('../assets/test_output.json', 'w') as f:
-      f.write(json.dumps(output))
+   sprite_priority = [[the_map.tile_is_sprite_priority(x, y) for x in range(MAX_INPUT_WIDTH)] for y in range(the_map.INPUT_HEIGHT)]
+   tile_priority = [[the_map.tile_is_high_priority(x, y) for x in range(MAX_INPUT_WIDTH)] for y in range(the_map.INPUT_HEIGHT)]
 
-   sprite_priority = [[tile_is_sprite_priority(x, y) for x in range(INPUT_WIDTH)] for y in range(INPUT_HEIGHT)]
-   print(sprite_priority)
+   output_dir = os.path.join('../assets/maps/', output_dir)
+   os.makedirs(output_dir, exist_ok=True)
+   with open(os.path.join(output_dir, 'tiles.json'), 'w') as f:
+      f.write(json.dumps(output))
+   with open(os.path.join(output_dir, 'info.json'), 'w') as f:
+      f.write(json.dumps({
+         'sprite_priority': sprite_priority,
+         'tile_priority': tile_priority,
+         'height': the_map.INPUT_HEIGHT,
+         'width': the_map.INPUT_WIDTH,
+         'y_offset': y_offset
+      }))
 
 def to_cpp_array(a_list: list[int]) -> str:
    return str(a_list).replace("[", "{").replace("]", "};")
